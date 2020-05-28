99674337f9b8f9f5940b4b11a6d817d0220b85dd
Reilly Beacom <contact@reillybeacom.net>
2020-05-28 15:00:31 +1000

    seprate json and csv

--- download.sh
+++ download.sh
@@ -1,14 +1,14 @@
-HOST="http://api.rainfallrecord.localhost"
+HOST="https://api.rainfallrecord.info"
 
 OPT="--silent --show-error"
 HEADER="Authorization: Token $API_KEY"
 
 curl -o "index.geo.json" "$HOST/locations.json" $OPT --header "$HEADER"
-curl -o "index.txt" "$HOST/locations.txt?_all=show" $OPT --header "$HEADER"
+curl -o "index.txt" "$HOST/locations.txt?_recent=show" $OPT --header "$HEADER"
 
 while read id
 do
     echo $id
-    curl -o "locations/$id.json" "$HOST/locations/$id.json" $OPT --header "$HEADER"
-    curl -o "locations/$id.csv" "$HOST/locations/$id.csv" $OPT --header "$HEADER"
+    curl -o "locations/$id.json" "$HOST/json/$id.json" $OPT --header "$HEADER"
+    curl -o "locations/$id.csv" "$HOST/csv/$id.csv" $OPT --header "$HEADER"
 done < index.txt
