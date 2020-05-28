b48bbd1014e6be7ab6fc296babe86ac5e2c85d65
Reilly Beacom <contact@reillybeacom.net>
2020-05-28 15:12:20 +1000

    change file path

--- download.sh
+++ download.sh
@@ -9,6 +9,6 @@ curl -o "index.txt" "$HOST/locations.txt?_recent=show" $OPT --header "$HEADER"
 while read id
 do
     echo $id
-    curl -o "locations/$id.json" "$HOST/json/$id.json" $OPT --header "$HEADER"
-    curl -o "locations/$id.csv" "$HOST/csv/$id.csv" $OPT --header "$HEADER"
+    curl -o "json/$id.json" "$HOST/locations/$id.json" $OPT --header "$HEADER"
+    curl -o "csv/$id.csv" "$HOST/locations/$id.csv" $OPT --header "$HEADER"
 done < index.txt
