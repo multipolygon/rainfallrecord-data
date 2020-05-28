57fc732ce997e022d9db3c532432391fb9ea0531
Reilly Beacom <contact@reillybeacom.net>
2020-05-28 14:51:33 +1000

    add csv

--- download.sh
+++ download.sh
@@ -1,10 +1,14 @@
-CURL="curl --silent --show-error"
+HOST="http://api.rainfallrecord.localhost"
 
-HOST="https://cache.rainfallrecord.info"
+OPT="--silent --show-error"
+HEADER="Authorization: Token $API_KEY"
 
-$CURL -o "index.geo.json" "$HOST/locations.json"
-$CURL -o "index.txt" "$HOST/locations.txt?_recent=show"
+curl -o "index.geo.json" "$HOST/locations.json" $OPT --header "$HEADER"
+curl -o "index.txt" "$HOST/locations.txt?_all=show" $OPT --header "$HEADER"
 
-while read id;
-do $CURL -o "locations/$id.json" "$HOST/locations/$id.json";
+while read id
+do
+    echo $id
+    curl -o "locations/$id.json" "$HOST/locations/$id.json" $OPT --header "$HEADER"
+    curl -o "locations/$id.csv" "$HOST/locations/$id.csv" $OPT --header "$HEADER"
 done < index.txt
