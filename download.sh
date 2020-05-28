9e426c511f0a6260a8ad0400d3a5cfffbdfc4707
Reilly Beacom <contact@reillybeacom.net>
2020-05-28 19:02:21 +1000

    -

--- download.sh
+++ download.sh
@@ -11,6 +11,7 @@ do
     echo $id
     curl -o "locations/$id.json" "$HOST/locations/$id.json" $OPT --header "$HEADER"
     curl -o "locations/$id.csv" "$HOST/locations/$id.csv" $OPT --header "$HEADER"
+    curl -o "locations/$id.xml" "$HOST/locations/$id.xml" $OPT --header "$HEADER"
 done < index.txt
 
 echo "Diff:"
