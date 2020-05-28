d09b1fde68cdf56dbf97f96a47df3b326d9ecc40
Reilly Beacom <contact@reillybeacom.net>
2020-05-28 15:25:35 +1000

    mv files (again!)

--- download.sh
+++ download.sh
@@ -1,14 +1,12 @@
-HOST="https://api.rainfallrecord.info"
-
 OPT="--silent --show-error"
 HEADER="Authorization: Token $API_KEY"
 
-curl -o "index.geo.json" "$HOST/locations.json" $OPT --header "$HEADER"
+curl -o "locations/index.json" "$HOST/locations.json" $OPT --header "$HEADER"
 curl -o "index.txt" "$HOST/locations.txt?_recent=show" $OPT --header "$HEADER"
 
 while read id
 do
     echo $id
-    curl -o "json/$id.json" "$HOST/locations/$id.json" $OPT --header "$HEADER"
-    curl -o "csv/$id.csv" "$HOST/locations/$id.csv" $OPT --header "$HEADER"
+    curl -o "locations/$id.json" "$HOST/locations/$id.json" $OPT --header "$HEADER"
+    curl -o "locations/$id.csv" "$HOST/locations/$id.csv" $OPT --header "$HEADER"
 done < index.txt
