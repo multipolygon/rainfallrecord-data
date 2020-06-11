52ca65edd4de30c51ff6ed45f6907f995fd31c8a
multipolygon <multipolygon@gmail.com>
2020-06-11 10:53:16 +1000

    update

--- download.sh
+++ download.sh
@@ -4,13 +4,16 @@ OPT="--silent --show-error"
 HEADER="Authorization: Token $API_KEY"
 
 curl -o "locations.json" "$HOST/locations.json" $OPT --header "$HEADER"
-curl -o "index.txt" "$HOST/locations.txt?_all=show" $OPT --header "$HEADER"
+curl -o "index.txt" "$HOST/locations.txt?_recent=show" $OPT --header "$HEADER"
 
 while read id
 do
-    echo $id
+    echo "$HOST/locations/$id"
+    echo ".json"
     curl -o "locations/$id.json" "$HOST/locations/$id.json" $OPT --header "$HEADER"
+    echo ".csv"
     curl -o "locations/$id.csv" "$HOST/locations/$id.csv" $OPT --header "$HEADER"
+    echo ".xml"
     curl -o "locations/$id.xml" "$HOST/locations/$id.xml" $OPT --header "$HEADER"
 done < index.txt
 
