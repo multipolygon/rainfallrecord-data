02437fa607c349194ae7c55561b5abbd0d36bbd5
multipolygon <multipolygon@gmail.com>
2020-10-11 13:53:23 +1100

    Add workflow_dispatch to github action workflow.

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@ echo "Download locations json:"
 curl -o "locations.json" "$HOST/locations.json" $OPT --header "$HEADER"
 
 echo "Download recent locations txt:"
-curl -o "locations.txt" "$HOST/locations.txt?_recent=show" $OPT --header "$HEADER"
+curl -o "recent.txt" "$HOST/locations.txt?_recent=show" $OPT --header "$HEADER"
 
 echo "Download:"
 while IFS="" read -r id || [ -n "$id" ]
@@ -19,7 +19,7 @@ do
     curl -o "locations/$id.csv" "$HOST/locations/$id.csv" $OPT --header "$HEADER"
     echo "xml"
     curl -o "locations/$id.xml" "$HOST/locations/$id.xml" $OPT --header "$HEADER"
-done < locations.txt
+done < recent.txt
 
 echo "Diff:"
 git diff --name-only
